6ee76e635500f0b65c71242bc60a47a348191099
serializedowen <wjh199455@gmail.com>
2016-02-13 15:19:18 -0500

    Added Chart.js library

--- src/popup.js
+++ src/popup.js
@@ -1,24 +1,14 @@
-
-var rule2 = {
-  conditions: [
-    new chrome.declarativeContent.PageStateMatcher({
-      pageUrl: { urlContains: 'amazon' },
-    })
-
-          // new chrome.declarativeContent.PageStateMatcher({
-          //   pageUrl: { hostEquals: 'www.google.com', schemes: ['https'] },
-          //   css: ["input[type='password']"]
-          // }),
-          // new chrome.declarativeContent.PageStateMatcher({
-          //   css: ["video"]
-          // })
-    ],
-    actions: [ new chrome.declarativeContent.ShowPageAction() ]
-};
-
-chrome.runtime.onInstalled.addListener(function(details) {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([rule2]);
-  });
-});
-      
+function draw() {
+var cv = document.getElementById("myChart");
+var ctx = cv.getContext("2d");
+ctx.fillStyle = "red";
+ctx.strokeStyle = "black";
+ctx.font='bold 15px Arial';
+  ctx.fillText("GameOver", 160, 300);
+  ctx.fillText("Your Score is: ", 145, 320);
+  ctx.strokeRect(100, 350, 50, 30);
+  ctx.strokeRect(250, 350, 50, 30);
+  ctx.fillStyle = "black";
+  ctx.fillText("Replay", 100, 370);
+  ctx.fillText("Exit", 260, 370);
+}
