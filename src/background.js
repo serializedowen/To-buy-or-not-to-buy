67d5525ce392b8ed8a825500e73e3d8bb4c6ae4a
serializedowen <wjh199455@gmail.com>
2016-02-13 22:46:42 -0500

    GGWP
    GGWP

--- src/background.js
+++ src/background.js
@@ -1,5 +1,5 @@
 
-var rule2 = {
+var rule = {
   conditions: [
     new chrome.declarativeContent.PageStateMatcher({
       pageUrl: { urlContains: 'amazon' },
@@ -18,7 +18,7 @@ var rule2 = {
 
 chrome.runtime.onInstalled.addListener(function(details) {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([rule2]);
+    chrome.declarativeContent.onPageChanged.addRules([rule]);
   });
 });
 
--- src/popup.js
+++ src/popup.js
@@ -1,14 +1,23 @@
-function draw() {
-var cv = document.getElementById("myChart");
-var ctx = cv.getContext("2d");
-ctx.fillStyle = "red";
-ctx.strokeStyle = "black";
-ctx.font='bold 15px Arial';
-  ctx.fillText("GameOver", 160, 300);
-  ctx.fillText("Your Score is: ", 145, 320);
-  ctx.strokeRect(100, 350, 50, 30);
-  ctx.strokeRect(250, 350, 50, 30);
-  ctx.fillStyle = "black";
-  ctx.fillText("Replay", 100, 370);
-  ctx.fillText("Exit", 260, 370);
+function draw(ctx) {
+  console.log('aaa');
+  // ctx.fillStyle = "red";
+  // ctx.strokeStyle = "black";
+  // ctx.font='bold 15px Arial';
+  // ctx.fillText("GameOver", 160, 300);
+  // ctx.fillText("Your Score is: ", 145, 320);
+  // ctx.strokeRect(100, 350, 50, 30);
+  // ctx.strokeRect(250, 350, 50, 30);
+  // ctx.fillStyle = "black";
+  // ctx.fillText("Replay", 100, 370);
+  // ctx.fillText("Exit", 260, 370);
+  // ctx.fill();
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  // document.querySelector('button').addEventListener('click', console.log('aaa'));
+  console.log('aaa');
+  var cv = document.getElementById("myChart");
+  var ctx = cv.getContext("2d");
+
+  draw(ctx);
+});
